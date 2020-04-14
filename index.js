ced432d1367c315412451fe702f5ad8d790d01d2
MaryHooker <mc020791@yahoo.com>
2020-04-14 08:51:11 -0500

    Mongoose/Create & Read

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ let port = 2112;
 //import json middleware
 app.use(express.json());
 
+//practically cope and paste everytime unless different user or database
+///////////////////////////////////////////////////////////
+//Database setup
+const mongoose = require('mongoose');
+//connection string under variable
+const mongoDB = 'mongodb+srv://Student:C0d3Cr3w@cluster0-p8fhv.mongodb.net/cs_database?retryWrites=true&w=majority';
+//Actually try and connect to the database
+//sanity
+console.log(`Connecting to Mongo at ${mongoDB}`)
+mongoose.connect(mongoDB, {useNewUrlParser: true,  useUnifiedTopology: true });
+//Pointer to database instance
+let db = mongoose.connection;
+//sanity
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+///////////////////////////////////////////////////////////
+
 //listen on port
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
@@ -20,7 +36,6 @@ let api = require('./routes/api.js')
 
 //mount route
 app.use('/api',api);
-
 
 //sanity
 app.get('/test',(req,res)=>{
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,9 @@
 let express = require('express');
 let router = express.Router();
 
+//Get reference to our model
+let ProductCollection = require('../models/ProductSchema');
+
 let productArray =
     [
         {
@@ -30,23 +33,51 @@ let productArray =
 //Read in CRUD
 //get all products
 router.get('/', (req, res) => {
-
-    res.send(productArray)
+    // res.send(productArray);
+    ProductCollection.find({},(errors,results)=>{
+        if(errors){
+            res.send(errors);
+        } else{
+            res.send(results);
+        }})
 })
 
 //Read in CRUD
 //get a specific product
 router.get('/:product_id', (req, res) => {
-
-    res.send(productArray[req.params.product_id])
+    // res.send(productArray[req.params.product_id]);
+    // ProductCollection.findById(req.params.product_id,
+    ProductCollection.findOne(
+        {productID:req.params.product_id},(errors,results)=>{
+            if(errors){
+                res.send(errors);
+            } else{
+                res.send(results);
+            }}
+    )
 })
 
 
 //Create in CRUD
-//add a product
+//add a new product
 router.post('/', (req, res) => {
-    req.body.status = 'CREATED';
-    res.send(req.body);
+    // req.body.status = 'CREATED';
+    // res.send(req.body);
+    ProductCollection.create(
+        {
+            productID: req.body.productID,
+            productName: req.body.productName,
+            productPrice:req.body.productPrice,
+            productQuantity: req.body.productQuantity
+        },(errors,results)=>{
+            if(errors){
+                res.send(errors);
+            } else{
+                res.send(results);
+            }
+        }
+    );
+    // res.send(`Product Created!`);
 })
 
 module.exports = router;
